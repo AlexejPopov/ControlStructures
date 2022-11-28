8adca67ca002ab0e1b2b16a727bf0936d414542f
Alexey Popov <Mr.AlexeyPopov@hotmail.com>
2022-11-28 19:18:03 +0100

    Update

--- Fibonacci2/Fibonacci2.cpp
+++ Fibonacci2/Fibonacci2.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 void main()
 {
-	setlocale(LC_ALL, "");
+	setlocale(LC_ALL, "rus");
 	int n;
 	cout << "Ââåäèòå ïðåäåëüíîå ÷èñëî: "; cin >> n;
 	for (int i = 0, a = 0, b = 1, c = a + b; i++ < n; c = (a = b) + (b = c))
